034927ff4c3856e94142593c956a382941d3366a
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-06-29 16:29:55 +0200

    + TQ84_DOT.pm

---
+++ tq84_dot/TQ84_DOT.pm
@@ -0,0 +1,132 @@
+package TQ84_DOT;
+use warnings;
+use strict;
+
+my $dot;
+my $filename_without_suffix;
+
+sub start { #_{
+
+  $filename_without_suffix = shift;
+  open ($dot, '>', "$filename_without_suffix.dot") or die;
+
+  print $dot 'digraph G {
+  
+    fontname="Helvetica";
+    node [shape=plaintext];
+
+    label="Bitcoin-Core: Wallet related classes"; labelloc=t;
+  
+  ';
+
+} #_}
+
+sub end { #_{
+
+  print $dot '}
+';
+
+} #_}
+
+sub open { #_{
+
+  my $out_type = 'pdf';
+
+  print system "dot $filename_without_suffix.dot -T$out_type -o$filename_without_suffix.$out_type\n";
+  system "okular $filename_without_suffix.$out_type";
+} #_}
+
+sub class { #_{
+  my $class_name = shift;
+print $dot <<DOT 
+  $class_name [
+    label=<
+      <table border="1" cellborder="0">
+        <tr><td align="left"><font face="Helvetica"><b>$class_name</b></font></td></tr>
+DOT
+
+} #_}
+
+sub file { #_{
+  my $filename = shift;
+print $dot <<DOT 
+      <tr><td align="left"><font face="Courier" point-size="8">$filename</font></td></tr>
+DOT
+
+} #_}
+
+sub attribute { #_{
+  my $attribute_name = shift;
+print $dot <<DOT 
+      <tr><td align="left" port="$attribute_name"><font face="Helvetica">$attribute_name</font></td></tr>
+DOT
+
+} #_}
+
+sub method { #_{
+  my $method_name = shift;
+print $dot <<DOT 
+      <tr><td align="left"><font face="Helvetica" color="blue">$method_name()</font></td></tr>
+DOT
+
+} #_}
+
+sub comment { #_{
+  my $comment = shift;
+print $dot <<DOT 
+      <tr><td align="left"><font face="Helvetica" color="green" point-size="9"><i>$comment</i></font></td></tr>
+DOT
+
+} #_}
+
+sub derives_from { #_{
+
+  my $derived_class = shift;
+  my $base_class    = shift;
+
+  print $dot "  $base_class -> $derived_class [arrowhead=invempty arrowtail=none dir=both];\n";
+
+} #_}
+
+sub link { #_{
+  my $from = shift;
+  my $to   = shift;
+
+  my %opts;
+  if (@_) {
+    
+    my $o = shift;
+    %opts = %$o;
+  }
+  $opts{dir} //= 'forward';
+
+  if ($opts{dir} eq 'back') {
+    ($to, $from) = ($from, $to);
+  }
+  my $arrowhead = '';
+  if ($opts{head}) {
+    $arrowhead = " arrowhead=$opts{head}";
+  }
+
+  print $dot "  $from -> $to [dir=$opts{dir}$arrowhead];\n";
+} #_}
+
+sub same_rank { #_{
+  print $dot "  {rank=same ";
+
+  print $dot "$_ " for @_;
+
+  print $dot "}\n";
+
+
+} #_}
+
+sub class_end { #_{
+print $dot <<DOT 
+      </table>
+    >
+  ];
+DOT
+
+} #_}
+1;
---
+++ tq84_dot/make-dot.pl
@@ -0,0 +1,141 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+
+use TQ84_DOT;
+
+TQ84_DOT::start("Wallet");
+
+TQ84_DOT::class("CAccountingEntry");
+TQ84_DOT::file("wallet/wallet.h");
+TQ84_DOT::comment("Internal transfers");
+TQ84_DOT::comment("DB key: acentry-account-counter");
+TQ84_DOT::class_end();
+
+TQ84_DOT::class("CMerkleTx");
+TQ84_DOT::file("wallet/wallet.h");
+TQ84_DOT::class_end();
+
+TQ84_DOT::class("CWalletTx");
+TQ84_DOT::file("wallet/wallet.h");
+TQ84_DOT::class_end();
+
+TQ84_DOT::class("CDB");
+TQ84_DOT::file("wallet/db.h");
+TQ84_DOT::comment("Interface to Berkeley DB");
+TQ84_DOT::method("Read");
+TQ84_DOT::method("Write");
+TQ84_DOT::attribute("env");
+TQ84_DOT::class_end();
+
+TQ84_DOT::class("CWalletDB");
+TQ84_DOT::file("wallet/walletdb.h");
+TQ84_DOT::attribute("batch");
+TQ84_DOT::method("LoadWallet");
+TQ84_DOT::class_end();
+
+
+TQ84_DOT::class("CKeyStore");
+TQ84_DOT::file("keystore.h");
+TQ84_DOT::class_end();
+
+TQ84_DOT::class("CBasicKeyStore");
+TQ84_DOT::file("keystore.h");
+TQ84_DOT::class_end();
+
+TQ84_DOT::class("CCryptoKeyStore");
+TQ84_DOT::file("wallet/crypter.h");
+TQ84_DOT::class_end();
+
+
+TQ84_DOT::class("CValidationInterface");
+TQ84_DOT::file("validationinterface.h");
+TQ84_DOT::class_end();
+
+
+TQ84_DOT::class("CWallet");
+TQ84_DOT::file("wallet/wallet.h");
+TQ84_DOT::attribute("dbw");
+TQ84_DOT::attribute("pwalletdbEncryption");
+TQ84_DOT::attribute("mapWallet");
+TQ84_DOT::attribute("laccentries");
+TQ84_DOT::method("CreateWalletFromFile");
+TQ84_DOT::class_end();
+
+TQ84_DOT::class("CWalletDBWrapper");
+TQ84_DOT::file("wallet/db.h");
+TQ84_DOT::attribute("env");
+TQ84_DOT::class_end();
+
+TQ84_DOT::class("CDBEnv");
+TQ84_DOT::file("wallet/db.h");
+TQ84_DOT::class_end();
+
+
+TQ84_DOT::derives_from('CWalletTx', 'CMerkleTx');
+TQ84_DOT::derives_from('CWallet', 'CCryptoKeyStore');
+TQ84_DOT::derives_from('CWallet', 'CValidationInterface');
+TQ84_DOT::derives_from('CBasicKeyStore', 'CKeyStore');
+TQ84_DOT::derives_from('CCryptoKeyStore', 'CBasicKeyStore');
+
+
+
+TQ84_DOT::link('CWallet:dbw'                , 'CWalletDBWrapper');
+TQ84_DOT::link('CWallet:pwalletdbEncryption', 'CWalletDB'       , {dir=>'back'});
+TQ84_DOT::link('CWalletDBWrapper:env'       , 'CDBEnv');
+TQ84_DOT::link('CDB:env'                    , 'CDBEnv');
+TQ84_DOT::link('CWalletDB:batch'            , 'CDB'             , {dir=>'back'});
+TQ84_DOT::link('CWallet:laccentries'        , 'CAccountingEntry', {head=>'crow'});
+TQ84_DOT::link('CWallet:mapWallet'          , 'CWalletTx'       , {head=>'crow'});
+
+
+TQ84_DOT::same_rank(qw(CDB CWallet CWalletDB CWalletDBWrapper CDBEnv));
+
+TQ84_DOT::end();
+TQ84_DOT::open();
+
+
+# my $filename_without_suffix = 'Wallet';
+# 
+# open (my $din, '<', "$filename_without_suffix.din") or die;
+# open (my $dot, '>', "$filename_without_suffix.dot") or die;
+
+# print $dot 'digraph G {
+# 
+#   fontname="Helvetica";
+#   node [shape=plaintext];
+# 
+# ';
+# 
+# my $in_class = 0;
+# while (my $in = <$din>) {
+# 
+#   if ($in =~ /^\s*$/) { #_{
+# 
+#      if ($in_class) {
+#         $in_class = 0;
+# 
+# print $dot"    >
+#   ];
+# ";
+#      }
+# 
+#      next;
+#   } #_}
+# 
+#   if ($in =~ /^(\w+)/) { #_{ new class
+#      my $classname = $1;
+#      $in_class = 1;
+# 
+# print $dot "  $classname [
+#     label=<
+#       <table border="1" cellcorder="0"
+#   ];
+# ";
+# 
+# 
+#   } #_}
+# }
+# 
+# 
+# print $dot "}\n";
