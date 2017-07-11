453b0759bf4589d8b67657f4dd5846548d8418f8
René Nyffenegger <mail@renenyffenegger.ch>
2017-07-11 19:56:08 +0200

    Using a few features of GraphViz::ClassDiagram

--- tq84_dot/todo.pl
+++ tq84_dot/todo.pl
@@ -7,98 +7,30 @@ use TQ84_DOT;
 TQ84_DOT::start("todo");
 TQ84_DOT::link("CCoinsViewDB:db", "CDBWrapper");
 
-TQ84_DOT::class("CDBWrapper");
-TQ84_DOT::file("dbwrapper.h");
-TQ84_DOT::comment("Wrapper for LevelDB?");
-TQ84_DOT::comment("Used for DATADIR/blocks/index and DATADIR/chainstate");
-TQ84_DOT::class_end();
-
-
-TQ84_DOT::class("CBlockFileInfo");
-TQ84_DOT::file("chain.h");
-TQ84_DOT::class_end();
-
-TQ84_DOT::class("CBlockTreeDB");
-TQ84_DOT::file("txdb.h");
-TQ84_DOT::comment("Access to the block database (DATADIR/blocks/index/)");
-TQ84_DOT::method("LoadBlockIndexGuts", {comment=>'Load mapBlockIndex'});
-TQ84_DOT::method("ReadLastBlockFile" , {comment=>'How many block info files are there?'});
-TQ84_DOT::method("ReadBlockFileInfo" , {comment=>'Read a specific CBlockFileInfo'});
-TQ84_DOT::method("Read/WriteFlag"    , {comment=>'...'});
-TQ84_DOT::class_end();
 
 TQ84_DOT::global_var("pblocktreedb", "CBlockTreeDB");
 
-TQ84_DOT::class("CBlockIndex");
-TQ84_DOT::file("chain.h");
-TQ84_DOT::comment("A Bitcoin block?");
-TQ84_DOT::comment("Apparently keeps track of a block.");
-TQ84_DOT::attribute("phashBlock", {comment=>"hash of block"});
-TQ84_DOT::attribute("pprev", {comment=>"hash of previous block"});
-TQ84_DOT::attribute("pskip");
-TQ84_DOT::class_end();
 
-TQ84_DOT::class("CDBBatch");
-TQ84_DOT::file("dbwrapper.h");
-TQ84_DOT::comment("Batch of changes queued to be written to a CDBWrapper");
-TQ84_DOT::attribute("parent");
-TQ84_DOT::class_end();
 
 TQ84_DOT::link("CBlockIndex:pprev", "CBlockIndex");
 TQ84_DOT::link("CBlockIndex:pskip", "CBlockIndex");
 TQ84_DOT::link("CDBBatch:parent", "CDBWrapper");
 
-TQ84_DOT::class("CCoinsView");
-TQ84_DOT::file("coins.h");
-TQ84_DOT::comment("Abstract view on the open txout dataset");
-TQ84_DOT::method("Cursor", {comment=>'Returns a CCoinsViewCursor'});
-TQ84_DOT::method("GetBestBlock", {comment=>'Retrieve the block hash whose state this CCoinsView currently represents'});
-TQ84_DOT::class_end();
 
-TQ84_DOT::class("CCoinsViewBacked");
-TQ84_DOT::file("coins.h");
-TQ84_DOT::class_end();
 
 TQ84_DOT::derives_from("CCoinsViewBacked", "CCoinsView");
 
-TQ84_DOT::class("CCoinsViewCache");
-TQ84_DOT::file("coins.h");
-TQ84_DOT::comment("adds a memory cache for transactions to another CCoinsView");
-TQ84_DOT::attribute("hashBlock");
-TQ84_DOT::class_end();
 
 TQ84_DOT::derives_from("CCoinsViewCache", "CCoinsViewBacked");
 
-TQ84_DOT::class("CCoinsViewCursor");
-TQ84_DOT::file("coins.h");
-TQ84_DOT::comment("Cursor for iterating over CoinsView state");
-TQ84_DOT::class_end();
 
-TQ84_DOT::class("CDBIterator");
-TQ84_DOT::file("dbwrapper.h");
-TQ84_DOT::attribute("parent");
-TQ84_DOT::method("GetKey");
-TQ84_DOT::class_end();
 
 TQ84_DOT::link("CDBIterator:parent", "CDBWrapper");
 
-TQ84_DOT::class("CCoinsViewDB");
-TQ84_DOT::file("txdb.h");
-TQ84_DOT::comment("CCoinsView backed by the coin database (chainstate/)");
-TQ84_DOT::comment("Compare CCoinsViewBacked.");
-TQ84_DOT::attribute("db");
-TQ84_DOT::class_end();
 
 TQ84_DOT::derives_from("CCoinsViewDB", "CCoinsView");
 
-TQ84_DOT::class("CDiskBlockIndex");
-TQ84_DOT::file("chain.h");
-TQ84_DOT::comment("Used to marshal pointers into hashes for db storage.");
-TQ84_DOT::class_end();
 
-TQ84_DOT::class("CDiskBlockPos");
-TQ84_DOT::file("chain.h");
-TQ84_DOT::class_end();
 
 TQ84_DOT::derives_from("CBlockTreeDB", "CDBWrapper");
 TQ84_DOT::derives_from("CDiskBlockIndex", "CBlockIndex");
@@ -106,12 +38,6 @@ TQ84_DOT::derives_from("CDiskBlockIndex", "CBlockIndex");
 
 TQ84_DOT::same_rank("CDBWrapper", "CDBBatch", "CCoinsViewDB", "CDBIterator");
 
-TQ84_DOT::class("CChain");
-TQ84_DOT::file("chain.h");
-TQ84_DOT::comment("An in-memory indexed chain of blocks");
-TQ84_DOT::attribute("vChain");
-TQ84_DOT::method("Genesis");
-TQ84_DOT::class_end();
 
 TQ84_DOT::end();
 TQ84_DOT::open();
--- tq84_dot/trx.pl
+++ tq84_dot/trx.pl
@@ -10,6 +10,83 @@ my $class_diagram = GraphViz::ClassDiagram->new("trx.pdf");
 
 $class_diagram->title("Bitcoin TRX classes");
 
-my $CChain = $class_diagram -> class('CChain');
+my $CDBWrapper       = $class_diagram -> class('CDBWrapper'      ); #_{
+
+   $CDBWrapper->file('dbwrapper.h');
+   $CDBWrapper->comment("Wrapper for LevelDB");
+   $CDBWrapper->comment("Used for DATADIR/blocks/index and DATADIR/chainstate");
+
+#_}
+my $CBlockFileInfo   = $class_diagram -> class('CBlockFileInfo'  ); #_{
+   $CBlockFileInfo -> file('chain.h');
+#_}
+my $CChain           = $class_diagram -> class('CChain'          ); #_{
+   $CChain->file('chain.h');  
+   $CChain->comment("An in-memory indexed chain of blocks");
+   $CChain->attribute("vChain");
+   $CChain->method("Genesis");
+#_}
+my $CBlockTreeDB     = $class_diagram -> class('CBlockTreeDB'    ); #_{
+   $CBlockTreeDB -> file('txdb.h');
+   $CBlockTreeDB -> comment("Access to the block database (DATADIR/blocks/index/)");
+   
+   $CBlockTreeDB -> method('LoadBlockIndexGuts', {comment=>'Load mapBlockIndex'});
+   $CBlockTreeDB -> method("ReadLastBlockFile" , {comment=>'How many block info files are there?'});
+   $CBlockTreeDB -> method("ReadBlockFileInfo" , {comment=>'Read a specific CBlockFileInfo'});
+   $CBlockTreeDB -> method("Read/WriteFlag"    , {comment=>'...'});
+
+#_}
+my $CBlockIndex      = $class_diagram -> class('CBlockIndex'     ); #_{
+   $CBlockIndex->file('chain.h');
+   $CBlockIndex->comment('A Bitcoin block?');
+   $CBlockIndex->comment('Apparently keeps track of a block');
+
+   $CBlockIndex->attribute('phashBlock', {comment=>'hash of block'});
+   $CBlockIndex->attribute('pprev'     , {comment=>'hash of previous block'});
+   $CBlockIndex->attribute('pskip');
+
+#_}
+my $CDBBatch         = $class_diagram -> class('CDBBatch'        ); #_{
+   $CDBBatch->comment('Batch of changes queued to be written to a CDBWrapper');
+   $CDBBatch->file('dbwrapper.h');
+   $CDBBatch->attribute('parent');
+#_}
+my $CCoinsView       = $class_diagram -> class('CCoinsView'      ); #_{
+   $CCoinsView->file('coins.h');
+   $CCoinsView->comment("Abstract view on the open txout dataset");
+   $CCoinsView->method("Cursor"      , {comment=>'Returns a CCoinsViewCursor', returns=>'CCoinsViewCursor'});
+   $CCoinsView->method("GetBestBlock", {comment=>'Retrieve the block hash whose state this CCoinsView currently represents'});
+#_}
+my $CCoinsViewBacked = $class_diagram -> class('CCoinsViewBacked'); #_{
+   $CCoinsViewBacked->file('coins.h');
+#_}
+my $CCoinsViewCache  = $class_diagram -> class('CCoinsViewCache' ); #_{
+   $CCoinsViewCache->file('coins.h');
+   $CCoinsViewCache->comment('adds a memory cache for transactions to another CCoinsView');
+   $CCoinsViewCache->attribute('hashBlock');
+#_}
+my $CCoinsViewCursor = $class_diagram -> class('CCoinsViewCursor'); #_{
+   $CCoinsViewCursor->file('coins.h');
+   $CCoinsViewCursor->comment("Cursor for iterating over CoinsView state");
+#_}
+my $CDBIterator      = $class_diagram -> class('CDBIterator'     ); #_{
+   $CDBIterator->file('dbwrapper.h');
+   $CDBIterator->attribute("parent");
+   $CDBIterator->method("GetKey");
+#_}
+my $CCoinsViewDB     = $class_diagram -> class('CCoinsViewDB'    ); #_{
+   $CCoinsViewDB->file('txdb.h');
+   $CCoinsViewDB->comment("CCoinsView backed by the coin database (chainstate/)");
+   $CCoinsViewDB->comment("Compare CCoinsViewBacked.");
+   $CCoinsViewDB->attribute("db");
+#_}
+my $CDiskBlockIndex  = $class_diagram -> class('CDiskBlockIndex' ); #_{
+   $CDiskBlockIndex->file('chain.h');
+   $CDiskBlockIndex->comment("Used to marshal pointers into hashes for db storage.");
+#_}
+my $CDiskBlockPos    = $class_diagram -> class('CDiskBlockPos'   ); #_{
+   $CDiskBlockPos->file('chain.h');  
+#_}
+   
 
 $class_diagram->create();
